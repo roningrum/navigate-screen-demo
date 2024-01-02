89114618c81eb71ec943faa01de141dd02f6b6e6
roningrum <rizkioktavianingrum@gmail.com>
2024-01-02 09:54:24 +0700

    details

--- src/screen/DetailScreen.js
+++ src/screen/DetailScreen.js
@@ -1,10 +1,11 @@
-import { Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
 import React from "react";
 
-const DetailScreen = () =>{
+const DetailScreen = ({navigation}) =>{
     return(
         <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
         <Text>Details Screen</Text>
+        <Button title='Go To Home' onPress={()=> navigation.navigate('Home')}></Button>
     </View>
     );
 };
